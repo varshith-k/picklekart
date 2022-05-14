8bedeae0ce0a16c47d392746c8abfa00529f7f31
Kaki Vishnu Sai Varshith <56587583+varshith-k@users.noreply.github.com>
2022-05-14 21:07:50 +0000

    Product base

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,19 @@ function Home() {
   return (
     <div className='home'>
        <div className='home_container'>
-           <img  src='public/images/PicklesBanner.png' alt="Banner"/>
+           <img  src='/images/PicklesBanner.png' alt="Banner"/>
+           <div className='home_row'>
+               {/* Product */}              
+               {/* Product */}
+           </div> 
+           <div className='home_row'>
+               {/* Product */}     
+                {/* Product */}                       
+               {/* Product */}
+           </div>
+           <div className='home_row'>
+               {/* Product */}              
+           </div>
        </div>
        </div>
   )
