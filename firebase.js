7b94703db2cb89b5d779f07b944c74b5860b3e1b
Kaki Vishnu Sai Varshith <vishnusaivarshith.k19@iiits.in>
2022-05-14 16:15:02 +0530

    Header - Picklekart push.

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAR67M9LbS8kKHN3W11pCbAY3dKDBhhG6M",
+  authDomain: "picklekart-69e55.firebaseapp.com",
+  projectId: "picklekart-69e55",
+  storageBucket: "picklekart-69e55.appspot.com",
+  messagingSenderId: "1090252754199",
+  appId: "1:1090252754199:web:e1996ca495df2c9d26fb52",
+  measurementId: "G-HS7BFVKF5V"
+};
---
+++ src/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+  return (
+    <div className='header'>
+        <img 
+        className='header_logo' 
+        src='/images/jarlogo.svg'
+        alt='Picklekart-Logo'></img>
+        <div
+        className='header_search'>
+            <input className='header_input' type="text"/>
+            {/* Logo */}
+        </div>
+        <div 
+        className='header_nav'>
+            <div className='header_option'>
+                 <span className='header_optionLineOne'>Hello Guest</span>
+                 <span className='header_optionLineTwe'>Sign In</span>
+            </div>
+            <div className='header_option'>
+            <span className='header_optionLineOne'>Returns</span>
+                 <span className='header_optionLineTwe'>Orders</span>
+            </div>
+            <div className='header_option'>
+            <span className='header_optionLineOne'>Your</span>
+                 <span className='header_optionLineTwe'>Kart</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
